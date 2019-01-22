87d704fe07f269ea0a4afc408670098b28ea2f7a
tyronejayy <tyronejoubert22@gmail.com>
2019-01-22 15:20:18 +0200

    basic php recp

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$myName = "Tyrone";
+
+$age = 21;
+
+$foods = array('lasagne', 'macaroni', 'roti&curry');
+
+echo " My name is " . $myName . " and my age is " . $age .  ". Currently my favourite foods are " . $foods[0]. ", " . $foods[1], " and " .$foods[2]. ".";
+
+
+?>
+    
+</body>
+</html>
